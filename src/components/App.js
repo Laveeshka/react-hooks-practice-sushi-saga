b2ebb94782487a76412e49b71de566f52a745e58
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-23 12:46:07 +1100

    render 4 sushis at a time

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  //set state for sushis list
+  const [sushis, setSushis] = useState([]);
+
+  //fetch sushis list and assign to state
+  useEffect(() => {
+    fetch(API)
+      .then(res => res.json())
+      .then(sushisData => {
+        setSushis(sushisData);
+      })
+  }, []);
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+//think it through Lavee
+//render 4 sushis at a time, hmm offset the array by 4 everytime
+//non-destructive method of modifying the sushis array
+//slice method
+//use state to update the moving start index
+function SushiContainer({ sushis }) {
+  const [startIndex, setStartIndex] = useState(0);
+  const offset = 4;
+
+  function showNextSushis(){
+    setStartIndex((prevState) => prevState + offset);
+    console.log(startIndex);
+  }
+
+  const sushiComponents = sushis
+    .slice(startIndex, startIndex + offset)
+    .map((sushi) => <Sushi key={sushi.id} sushi={sushi} />);  
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton showNextSushis={showNextSushis}/>
     </div>
   );
 }
